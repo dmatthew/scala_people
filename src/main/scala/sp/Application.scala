75af397b40e83d73a2f4bba6c0daf6fe2cdb487c
Matt Davis <madavis111@outlook.com>
2017-07-08 15:51:59 -0700

    Implement list command

--- src/main/scala/sp/Application.scala
+++ src/main/scala/sp/Application.scala
@@ -34,13 +34,16 @@ object Application {
     }
   }
 
+  /**
+   * Create the people table if its not already created.
+   */
   def createTables(): Unit = {
     Class.forName("org.postgresql.Driver").newInstance
     val connection_string = "jdbc:postgresql://192.168.41.20/scala_people?user=postgres&password=password"
     val conn:Connection = DriverManager.getConnection(connection_string)
     try {
       val statement = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
-      val result = statement.executeQuery("CREATE TABLE IF NOT EXISTS people (id serial primary key, name VARCHAR(255), age int, occupation VARCHAR(255))")
+      val result = statement.executeUpdate("CREATE TABLE IF NOT EXISTS people (id serial primary key, name VARCHAR(255), age int, occupation VARCHAR(255))")
     } catch {
       case e: Throwable => println("Could not connect")
     } finally {
@@ -75,5 +78,25 @@ object Application {
 
   def promptListPeople(): Unit = {
     println("You chose to list all people")
+
+    Class.forName("org.postgresql.Driver").newInstance
+    val connection_string = "jdbc:postgresql://192.168.41.20/scala_people?user=postgres&password=password"
+    val conn:Connection = DriverManager.getConnection(connection_string)
+    try {
+      val statement = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
+      val result = statement.executeQuery("SELECT * FROM people")
+      if (!result.isBeforeFirst) {
+        println("There are no people to list")
+      }
+      else {
+        while (result.next) {
+          println(result.getString("name").padTo(20, ' ') + " | " + result.getString("age").padTo(3, ' ') + " | " + result.getString("occupation"))
+        }
+      }
+    } catch {
+      case e: Throwable => println("Error: " + e)
+    } finally {
+      conn.close
+    }
   }
 }
